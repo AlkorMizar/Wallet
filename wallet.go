2aaa40ee98d05ddd311374999125082ef52281c1
AlkorMizar <anisa15012002@gmail.com>
2022-03-22 00:44:37 +0300

    added implementation of deposite, withdraw, get balance

--- wallet.go
+++ wallet.go
@@ -1,5 +1,9 @@
 package wallet
 
+import (
+	"errors"
+)
+
 type Bitcoin float64
 
 type Wallet struct {
@@ -9,16 +13,29 @@ type Wallet struct {
 // Deposite on this wallet.
 //bit >= 0
 func (w *Wallet) Deposit(bit Bitcoin) (e error) {
+	if bit < 0 {
+		return errors.New("can't process negative deposite")
+	}
+	w.balance += bit
 	return nil
 }
 
 // Deposite on this wallet.
-//bit >= 0
+//bit >= 0 and can't be bigger than wallets balance
 func (w *Wallet) Withdraw(bit Bitcoin) (e error) {
+	if bit < 0 {
+		return errors.New("can't process negative withdraw")
+	}
+
+	if w.balance-bit < 0 {
+		return errors.New("balance less then withdraw amount")
+	}
+
+	w.balance -= bit
 	return nil
 }
 
 //Get balance of this wallet
 func (w Wallet) Balance() (res Bitcoin) {
-	return -1
+	return w.balance
 }
