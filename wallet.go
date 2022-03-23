484b675c1073312dedc2a7fe1ab65f1ef25fdbc5
AlkorMizar <anisa15012002@gmail.com>
2022-03-23 14:17:09 +0300

    Added errors

--- wallet.go
+++ wallet.go
@@ -4,38 +4,47 @@ import (
 	"errors"
 )
 
+var (
+	ErrNegDeposite        = errors.New("negative deposite")
+	ErrNegWithdraw        = errors.New("negative withdraw")
+	ErrNotEnoughOnBalance = errors.New("balance less then withdraw amount")
+)
+
 type Bitcoin float64
 
 type Wallet struct {
 	balance Bitcoin
 }
 
-//Get balance of this wallet
+// Balance get balance of this wallet
 func (w Wallet) Balance() (res Bitcoin) {
 	return w.balance
 }
 
 // Deposite on this wallet.
-//bit >= 0
-func (w *Wallet) Deposit(bit Bitcoin) (e error) {
+// bit >= 0
+func (w *Wallet) Deposit(bit Bitcoin) error {
 	if bit < 0 {
-		return errors.New("can't process negative deposite")
+		return ErrNegDeposite
 	}
+
 	w.balance += bit
+
 	return nil
 }
 
-// Deposite on this wallet.
-//bit >= 0 and can't be bigger than wallets balance
-func (w *Wallet) Withdraw(bit Bitcoin) (e error) {
+// Withdraw from this wallet.
+// bit >= 0 and can't be bigger than wallets balance
+func (w *Wallet) Withdraw(bit Bitcoin) error {
 	if bit < 0 {
-		return errors.New("can't process negative withdraw")
+		return ErrNegWithdraw
 	}
 
 	if w.balance-bit < 0 {
-		return errors.New("balance less then withdraw amount")
+		return ErrNotEnoughOnBalance
 	}
 
 	w.balance -= bit
+
 	return nil
 }
