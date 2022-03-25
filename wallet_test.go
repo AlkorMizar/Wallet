7d29e18eb63080d6d903ae0b32c2a644436dbabb
AlkorMizar <anisa15012002@gmail.com>
2022-03-26 00:57:13 +0300

    fix test

--- wallet_test.go
+++ wallet_test.go
@@ -2,6 +2,7 @@ package wallet_test
 
 import (
 	"errors"
+	"math"
 	"sync"
 	"testing"
 
@@ -160,8 +161,8 @@ func TestCocurrency(t *testing.T) {
 	}
 
 	wg.Wait()
-
-	if w.Balance() != want {
+	tolerance := 0.00001
+	if diff := math.Abs(float64(w.Balance() - want)); diff > tolerance {
 		t.Fatalf("erorr in test want: %f, got: %f", want, w.Balance())
 	}
 }
